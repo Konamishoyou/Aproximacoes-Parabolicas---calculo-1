8eb35180971c2415109510faae4c7d7902ee1f3f
Konamisouza <luanesoliveira520@gmail.com>
2025-12-12 00:04:15 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TrabalhoCalculo from './components/TrabalhoCalculo';
+
+function App() {
+  return (
+    <div className="App">
+      <TrabalhoCalculo />
+    </div>
+  );
+}
+
+export default App;
